7c2239032793d5bb455a902d82a7ebf3872fa04c
Vitalik <vitalikgermak6@gmail.com>
2024-12-26 12:25:14 +0200

    add new style

--- index.js
+++ index.js
@@ -13,7 +13,6 @@ let nextHideParagraph = document.querySelectorAll(".hide-paragraph");
 
 whiteCircle.forEach((item, index) => {
   item.addEventListener("click", () => {
-    // Закрити всі параграфи, крім того, на який натиснули
     hideParagraph.forEach((paragraph, i) => {
       if (i !== index) {
         paragraph.classList.remove("active");
@@ -22,14 +21,13 @@ whiteCircle.forEach((item, index) => {
       }
     });
 
-    // Тогл для натиснутого параграфа
     hideParagraph[index].classList.toggle("active");
     nextHideParagraph[index].classList.toggle("transform");
     whiteCircle[index].classList.toggle("none-rotate");
   });
 });
 function startSharedTimer(durationInSeconds) {
-  const timerElements = document.querySelectorAll(".timer"); // Всі елементи з класом "timer"
+  const timerElements = document.querySelectorAll(".timer");
 
   let remainingTime = durationInSeconds;
 
@@ -38,28 +36,26 @@ function startSharedTimer(durationInSeconds) {
     const minutes = Math.floor((remainingTime % 3600) / 60);
     const seconds = remainingTime % 60;
 
-    // Оновлюємо всі таймери на сторінці
     timerElements.forEach((timer, index) => {
       if (index % 3 === 0) {
-        timer.textContent = String(hours).padStart(2, "0"); // Години
+        timer.textContent = String(hours).padStart(2, "0");
       } else if (index % 3 === 1) {
-        timer.textContent = String(minutes).padStart(2, "0"); // Хвилини
+        timer.textContent = String(minutes).padStart(2, "0");
       } else if (index % 3 === 2) {
-        timer.textContent = String(seconds).padStart(2, "0"); // Секунди
+        timer.textContent = String(seconds).padStart(2, "0");
       }
     });
 
     if (remainingTime > 0) {
       remainingTime--;
     } else {
-      clearInterval(timerInterval); // Зупинити таймер після завершення
+      clearInterval(timerInterval);
       alert("Таймер завершився!");
     }
   }
 
-  updateTimer(); // Початкове оновлення
+  updateTimer();
   const timerInterval = setInterval(updateTimer, 1000);
 }
 
-// Запускаємо таймер на 25 годин (1 день і 1 година)
 startSharedTimer(25 * 3600);
