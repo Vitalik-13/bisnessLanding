e287289a36813a2058e036c0e5cc54786a5a81ac
Vitalik <vitalikgermak6@gmail.com>
2024-12-26 10:31:38 +0200

    add new style

--- index.js
+++ index.js
@@ -6,3 +6,25 @@ let swiper = new Swiper(".mySwiper", {
     prevEl: ".swiper-button-prev",
   },
 });
+
+let whiteCircle = document.querySelectorAll(".white-circle");
+let hideParagraph = document.querySelectorAll(".nine-section-item");
+let nextHideParagraph = document.querySelectorAll(".hide-paragraph");
+
+whiteCircle.forEach((item, index) => {
+  item.addEventListener("click", () => {
+    // Закрити всі параграфи, крім того, на який натиснули
+    hideParagraph.forEach((paragraph, i) => {
+      if (i !== index) {
+        paragraph.classList.remove("active");
+        nextHideParagraph[i].classList.remove("transform");
+        whiteCircle[i].classList.remove("none-rotate");
+      }
+    });
+
+    // Тогл для натиснутого параграфа
+    hideParagraph[index].classList.toggle("active");
+    nextHideParagraph[index].classList.toggle("transform");
+    whiteCircle[index].classList.toggle("none-rotate");
+  });
+});
